ba56c78ea6b851b2dc777f90ebccf1965db18edf
dishantsethi <dishantsethi14@gmail.com>
2023-01-14 23:19:50 +0530

    Script fix

---
+++ deploy_front.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e
+
+cd prod_front/
+
+npm run build
+
+cd dist
+
+git add . -f
+git commit -m "Deployment $(date)"
+git push -f git@github.com:dishantsethi/prodinit.git master:gh-pages
