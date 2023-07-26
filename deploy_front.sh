2eb1a0e7aa02678aa21d299563a911085999bed0
dishantsethi <dishantsethi14@gmail.com>
2023-07-26 23:03:51 +0530

    spell check

--- deploy_front.sh
+++ deploy_front.sh
@@ -6,9 +6,16 @@ git checkout gh-pages
 git reset --hard origin/master
 
 rm -rf prod_back terraform deploy_front.sh README.md prod_front
-cd frontend 
-mv ./* ../..
-cd ../..  
+cp ./frontend/index.html ./
+cp ./frontend/PRODINIT.png ./
+mkdir assets/css
+mkdir assets/js
+mkdir assets/images
+
+cp ./frontend/assets/css/* ./assets/css/
+cp ./frontend/assets/js/* ./assets/js/
+cp ./frontend/assets/images/* ./assets/images/
+
 rm -rf frontend 
 
 echo 'www.prodinit.com' > CNAME
