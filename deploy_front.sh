c74f539711cde5106392e456afb7a9a3c34f90b8
dishantsethi <dishantsethi14@gmail.com>
2023-10-06 13:43:18 +0530

    remove unnecessary files and update deploy script

--- deploy_front.sh
+++ deploy_front.sh
@@ -5,9 +5,11 @@ set -e
 git checkout gh-pages
 git reset --hard origin/master
 
-rm -rf prod_back terraform deploy_front.sh README.md prod_front
+rm -rf deploy_front.sh README.md prod_front
 cp ./frontend/index.html ./
 cp ./frontend/PRODINIT.png ./
+cp ./frontend/PRODINIT.ico ./
+
 mkdir assets
 mkdir assets/css
 mkdir assets/js
@@ -24,3 +26,5 @@ echo 'www.prodinit.com' > CNAME
 git add .
 git commit -m "Deployment $(date)"
 git push -f origin gh-pages
+
+git checkout master
